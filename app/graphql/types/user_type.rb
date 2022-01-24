c04fd5ea46e6b9dc80e5b9186d2226f66b5561c3
juliocabrera820 <juliocabrera820@gmail.com>
2022-01-23 19:43:56 -0600

    retrieve all absences from today

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :email, String, null: true
-    field :company_id, Integer, null: false
+    field :absences, [Types::AbsenceType], null: true
+    field :attendances, [Types::AttendanceType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
